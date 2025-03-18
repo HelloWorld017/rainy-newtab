0ba883ea946c77a253dc83e84a87d84e783e8754
nenw <khi@nenw.dev>
2025-03-19 00:03:57 +0900

    revert: use web extension vite plugin
    This reverts commit 5f044e4f981a2a06399c33766ef4fbb312cb78ac.

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,34 @@
 import { resolve } from 'node:path';
 import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
-import webExtension from 'vite-plugin-web-extension';
-import svgLoader from 'vite-svg-loader';
 
-const isExtension = process.env.IS_EXTENSION === 'true';
+import svgLoader from 'vite-svg-loader';
 
 export default defineConfig({
 	build: {
 		emptyOutDir: true,
 		target: ['chrome130', 'firefox130'],
+		rollupOptions: {
+			input: {
+				app: resolve(__dirname, 'index.html'),
+				worker: resolve(__dirname, 'extension/worker.ts'),
+			},
+			output: {
+				entryFileNames: chunk =>
+					chunk.name === 'worker' ? '[name].js' : 'assets/[name]-[hash].js',
+				assetFileNames: 'assets/[name]-[hash][extname]',
+			},
+		},
 	},
 	esbuild: {
 		jsx: 'automatic',
 		jsxImportSource: 'vue',
 	},
-	plugins: [vue(), svgLoader(), ...(isExtension ? [webExtension()] : [])],
+	plugins: [vue(), svgLoader()],
 	resolve: {
 		alias: [{ find: /^@\//, replacement: `${resolve(__dirname, './app')}/` }],
 	},
 	define: {
-		__EXTENSION__: isExtension,
+		__EXTENSION__: process.env.IS_EXTENSION === 'true' ? true : false,
 	},
 });
